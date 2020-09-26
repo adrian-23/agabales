a091f79d63e4955ba7b16c3034abc46897d3e7a3
gabalea <gabalea@wwu.edu>
2020-09-26 12:30:05 -0700

    Updates

---
+++ precache-manifest.21f9f9a6024aaadb53d3ada0744360bb.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301f5a4232f3b1da4efb7b905fb779d0",
+    "url": "/agabales/index.html"
+  },
+  {
+    "revision": "adf685424341a80c0e7e",
+    "url": "/agabales/static/css/main.b9bcad3f.chunk.css"
+  },
+  {
+    "revision": "f9241e8ec6e82779074b",
+    "url": "/agabales/static/js/2.2a85ead1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/agabales/static/js/2.2a85ead1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf685424341a80c0e7e",
+    "url": "/agabales/static/js/main.5df27ac1.chunk.js"
+  },
+  {
+    "revision": "605de06c2c570b2ad4bc",
+    "url": "/agabales/static/js/runtime-main.60603778.js"
+  },
+  {
+    "revision": "dca34bcf8d1e9ff64b2dbc08c6c8a42b",
+    "url": "/agabales/static/media/B261.dca34bcf.jpg"
+  },
+  {
+    "revision": "2a5132d6553488a4bedcdc3b839c92d3",
+    "url": "/agabales/static/media/LB1.2a5132d6.png"
+  },
+  {
+    "revision": "64e633deb2173cc886cf9e7edc1a4069",
+    "url": "/agabales/static/media/LB10.64e633de.png"
+  },
+  {
+    "revision": "47c49bce51518490c06db05f76275023",
+    "url": "/agabales/static/media/LB11.47c49bce.png"
+  },
+  {
+    "revision": "9b2d6d89199a00e69eed1ea19b1ad8c4",
+    "url": "/agabales/static/media/LB12.9b2d6d89.png"
+  },
+  {
+    "revision": "eac67e7a10f1bb35202d0c0f331da6a5",
+    "url": "/agabales/static/media/LB2.eac67e7a.png"
+  },
+  {
+    "revision": "0ed040bc41f50231eefa81815220a500",
+    "url": "/agabales/static/media/LB3.0ed040bc.png"
+  },
+  {
+    "revision": "837604cbee74c04f564857e3987036cf",
+    "url": "/agabales/static/media/LB4.837604cb.png"
+  },
+  {
+    "revision": "b1276181f562a613dffaec68f36e984f",
+    "url": "/agabales/static/media/LB5.b1276181.png"
+  },
+  {
+    "revision": "441c426d2a4363f47973b3017e1ba3bf",
+    "url": "/agabales/static/media/LB6.441c426d.png"
+  },
+  {
+    "revision": "b272ac4bec11d9e39e26a54142ed4eaf",
+    "url": "/agabales/static/media/LB7.b272ac4b.png"
+  },
+  {
+    "revision": "78837ada2b8168a0b0b9048621be9fca",
+    "url": "/agabales/static/media/LB8.78837ada.png"
+  },
+  {
+    "revision": "a27e5cd26ff70c217ed062c76143f4e4",
+    "url": "/agabales/static/media/LB9.a27e5cd2.png"
+  },
+  {
+    "revision": "2fc352d3e48d8905a558534ec513f349",
+    "url": "/agabales/static/media/MSHELL.2fc352d3.png"
+  },
+  {
+    "revision": "c7cff9d33568b79ac033834934c97d4a",
+    "url": "/agabales/static/media/Resume.c7cff9d3.pdf"
+  },
+  {
+    "revision": "834343b5c6178bd51a8fc8700a7546f9",
+    "url": "/agabales/static/media/Right arrow.834343b5.png"
+  },
+  {
+    "revision": "5c38dc8c30fd328e55e1c7a2a03f6b91",
+    "url": "/agabales/static/media/YTS1.5c38dc8c.png"
+  },
+  {
+    "revision": "fd648ca361b38b7c25f0b0e7d2fedc83",
+    "url": "/agabales/static/media/YTS3.fd648ca3.png"
+  },
+  {
+    "revision": "198e4263c339e98f5ebf46bb7c1b1b0a",
+    "url": "/agabales/static/media/YTS4.198e4263.png"
+  },
+  {
+    "revision": "927bfbafe9525f0ed8fe5f10dc075df1",
+    "url": "/agabales/static/media/YTS5.927bfbaf.png"
+  },
+  {
+    "revision": "0271ecf039bd0049a2a68a7bacf2d70b",
+    "url": "/agabales/static/media/mshell2.0271ecf0.png"
+  },
+  {
+    "revision": "98f104033604d6a6c1726f23beff0629",
+    "url": "/agabales/static/media/mshell3.98f10403.png"
+  },
+  {
+    "revision": "143d69f168718de3a958be6c6793ef2d",
+    "url": "/agabales/static/media/mshell4.143d69f1.png"
+  },
+  {
+    "revision": "ef4d40230bb724181e35117501cfe309",
+    "url": "/agabales/static/media/mshell5.ef4d4023.jpg"
+  },
+  {
+    "revision": "7dbc0f70cc9d463c9466072df7d8f8fa",
+    "url": "/agabales/static/media/webpic.7dbc0f70.png"
+  }
+]);
